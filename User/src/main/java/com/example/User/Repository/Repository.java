a617bbf593667e25c5cb901881030c89c2731cd5
Puneeth9948 <pxa32960@ucmo.edu>
2025-06-12 21:25:34 -0500

    Delete User directory

--- User/src/main/java/com/example/User/Repository/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.User.Repository;
-
-
-import com.example.User.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-@org.springframework.stereotype.Repository
-public interface Repository extends JpaRepository<User, Long> {
-
-}
